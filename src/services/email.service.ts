38bfe2abb44997f68b6440009fde886a58c7544e
Sanyi81 <skyharts2002@gmail.com>
2024-10-06 13:57:51 +0300

    lesson 7

---
+++ src/services/email.service.ts
@@ -0,0 +1,46 @@
+import path from "node:path";
+
+import nodemailer, { Transporter } from "nodemailer";
+import hbs from "nodemailer-express-handlebars";
+
+import { configs } from "../configs/config";
+import { emailConstants } from "../constants/email.constants";
+import { EmailTypeEnum } from "../enums/email-type.enum";
+
+class EmailService {
+  private transporter: Transporter;
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: configs.SMTP_EMAIL,
+        pass: configs.SMTP_PASSWORD,
+      },
+    });
+
+    const hbsOptions = {
+      viewEngine: {
+        extname: ".hbs",
+        partialsDir: path.join(process.cwd(), "src", "template", "partials"),
+        layoutsDir: path.join(process.cwd(), "src", "template", "layouts"),
+      },
+      viewPath: path.join(process.cwd(), "src", "template", "views"),
+      extName: ".hbs",
+    };
+
+    this.transporter.use("compile", hbs(hbsOptions));
+  }
+
+  public async sendMail(to: string, type: EmailTypeEnum): Promise<void> {
+    const { subject, template } = emailConstants[type];
+    const options = {
+      to,
+      subject,
+      template,
+    };
+    await this.transporter.sendMail(options);
+  }
+}
+
+export const emailService = new EmailService();
